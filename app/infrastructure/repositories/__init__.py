72031310bd73a7e5a800ec4b30683b2b5daf6c60
Alejandro Atria <alejandro.atria@enviame.io>
2025-06-21 12:40:06 -0500

    create sqlalchemy task repository

--- app/infrastructure/repositories/__init__.py
+++ app/infrastructure/repositories/__init__.py
@@ -1,1 +1,2 @@
 from .task_list import SQLAlchemyTaskListRepository
+from .task import SQLAlchemyTaskRepository
---
+++ app/infrastructure/repositories/task.py
@@ -0,0 +1,82 @@
+from datetime import datetime
+from sqlalchemy import select
+from typing import List, Optional
+from sqlalchemy.orm import Session
+
+from app.domain.entities import Task
+from app.domain.interfaces import ITaskRepository
+from app.infrastructure.db.models.task import TaskModel
+from app.infrastructure.utils.sqlalchemy_filters_parser import parse_filters
+
+
+class SQLAlchemyTaskRepository(ITaskRepository):
+    
+    def __init__(
+        self,
+        session: Session,
+    ):
+        self.db = session      
+
+    async def _find_by_id(self, session, id: int):
+        stmt = select(TaskModel).where(
+            TaskModel.id == id,
+            TaskModel.deleted_at.is_(None),
+        )
+        result = await session.execute(stmt)
+        return result.scalar_one_or_none()
+    
+    async def get(self, id: int) -> Optional[Task]:
+        async with self.db() as session:
+            task_model = await self._find_by_id(session, id)
+            return task_model.to_entity() if task_model else None
+    
+    async def create(self, task_in: Task) -> Task:
+        async with self.db() as session:
+            task_model = TaskModel.from_entity(task_in)
+            session.add(task_model)
+            await session.commit()
+            await session.refresh(task_model)
+            return task_model.to_entity()
+    
+    async def update(self, task_id: int, update_values: dict) -> Task:
+        async with self.db() as session:
+            task_model = await self._find_by_id(task_id)
+            if not task_model:
+                return None
+            
+            for key, value in update_values.items():
+                setattr(task_model, key, value)
+                
+            await session.commit()
+            await session.refresh(task_model)
+            
+            return task_model.to_entity()
+    
+    async def delete(self, task_id: int) -> bool:
+        async with self.db() as session:
+            task_model = await self._find_by_id(task_id)
+            if not task_model:
+                return False
+            
+            task_model.deleted_at = datetime.now()
+            await session.commit()
+            
+            return True
+
+    async def list_by_task_list(self, task_list_id: int, *, filters: dict = None) -> List[Task]:
+        
+        aditional_filters = parse_filters(filters) if filters else []
+        
+        async with self.db() as session:
+            stmt = (
+                select(TaskModel)
+                .where(
+                    TaskModel.task_list_id == task_list_id,
+                    TaskModel.deleted_at.is_(None),
+                )
+                .where(*aditional_filters)
+            )
+            
+            result = await session.execute(stmt)
+            tasks_models = result.scalars().all()
+            return [task_model.to_entity() for task_model in tasks_models if task_model]
