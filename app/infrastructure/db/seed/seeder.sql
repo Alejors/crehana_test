525750e1b1307cf13620ddbd2ce582f3dce76da2
Alejandro Atria <alejandro.atria@enviame.io>
2025-06-21 18:07:51 -0500

    seeder for base data

---
+++ app/infrastructure/db/seed/seeder.sql
@@ -0,0 +1,25 @@
+INSERT IGNORE INTO users (id, username, email, password)
+VALUES 
+  (1, 'user1', 'user1@example.com', '$2b$12$F1DlWKoiKLMMnjT/n0Q56eedZK/lmUvxGk3EO5QYI55VXYuNYTZ2y'),
+  (2, 'user2', 'user2@example.com', '$2b$12$F1DlWKoiKLMMnjT/n0Q56eedZK/lmUvxGk3EO5QYI55VXYuNYTZ2y'),
+  (3, 'user3', 'user3@example.com', '$2b$12$F1DlWKoiKLMMnjT/n0Q56eedZK/lmUvxGk3EO5QYI55VXYuNYTZ2y'),
+  (4, 'user4', 'user4@example.com', '$2b$12$F1DlWKoiKLMMnjT/n0Q56eedZK/lmUvxGk3EO5QYI55VXYuNYTZ2y');
+
+INSERT IGNORE INTO task_lists (id, name)
+VALUES 
+  (1, 'list1'),
+  (2, 'list2'),
+  (3, 'list3');
+
+INSERT IGNORE INTO tasks (id, description, task_list_id, is_completed, priority)
+VALUES
+  (1, 'Tarea 1 de list1', 1, false, 'low'),
+  (2, 'Tarea 2 de list1', 1, true,  'medium'),
+  (3, 'Tarea 3 de list1', 1, false, 'high'),
+
+  (4, 'Tarea 1 de list2', 2, true,  'medium'),
+  (5, 'Tarea 2 de list2', 2, false, 'low'),
+
+  (6, 'Tarea 1 de list3', 3, true,  'medium'),
+  (7, 'Tarea 2 de list3', 3, true,  'high'),
+  (8, 'Tarea 3 de list3', 3, false, 'medium');
