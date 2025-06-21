174f617f2cff6d0d453950493cef0a234787bad1
Alejandro Atria <alejandro.atria@enviame.io>
2025-06-20 20:26:53 -0500

    db connection

---
+++ app/core/config.py
@@ -0,0 +1,24 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    MYSQL_USER: str
+    MYSQL_PASSWORD: str
+    MYSQL_HOST: str
+    MYSQL_PORT: str
+    MYSQL_DB: str
+
+    @property
+    def db_url(self):
+        return (
+            f"mysql+mysqlconnector://{self.MYSQL_USER}:"
+            f"{self.MYSQL_PASSWORD}@{self.MYSQL_HOST}:"
+            f"{self.MYSQL_PORT}/{self.MYSQL_DB}"
+        )
+
+    class Config:
+        env_file = ".env"
+        extra = "ignore"
+
+
+settings = Settings()
