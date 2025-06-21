d6879f1731a44d0fc042072d989420d284466dff
Alejandro Atria <alejandro.atria@enviame.io>
2025-06-21 18:07:43 -0500

    include password validator on regitration DTO only

--- app/domain/schemas/user.py
+++ app/domain/schemas/user.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, validator
 
 from app.domain.entities import User
 
@@ -13,6 +13,16 @@ class UserBase(BaseModel):
 
 class UserCreate(UserBase):
     username: str
+
+    @validator("password")
+    def validate_password(cls, value):
+        if len(value) < 8:
+            raise ValueError("Password must be at least 8 characters long")
+        if not any(c.isupper() for c in value):
+            raise ValueError("Password must contain at least one uppercase letter")
+        if not any(c.isdigit() for c in value):
+            raise ValueError("Password must contain at least one number")
+        return value
 
     def to_entity(self) -> User:
         return User(email=self.email, password=self.password, username=self.username)
